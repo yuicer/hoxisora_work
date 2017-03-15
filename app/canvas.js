0fe8254c6e1fef0a7104de7c542e19d8c998e404
yuicer <yuicer@yeah.net>
2017-03-15 18:19:14 +0800

    3.15

--- app/canvas.js
+++ app/canvas.js
@@ -12,7 +12,15 @@ var hoxisora = {
     width: {},
     height: {},
     rAF: {},
-    star_count: 2,
+    //star
+    star_count: 100,
+    star_maxR: 2.2,
+    star_speed: 200,
+    stars: [],
+    //meteor
+    meteor_speed: 1,
+    meteor_frame: 0,
+    time: new Date(),
     init: function () {
         var me = this;
         me.canvas = $("hosisora");
@@ -21,15 +29,47 @@ var hoxisora = {
         me.height = window.innerHeight;
         me.canvas.width = window.innerWidth;
         me.canvas.height = window.innerHeight;
-        //
-        me.draw_bg();
+        me.new_star();
+
         //anime loop
-        //        me.loop();
-        //        me.draw_star();
+        me.loop();
+        //        me.draw_bg();
+        //        var meteor = me.new_meteor();
+        //        me.draw_meteor(meteor);
+    },
+    new_star: function () {
+        var me = this;
+        for (let i = 0; i < me.star_count; i++) {
+            var a = {};
+            a.x = Math.floor(Math.random() * me.height) - (me.height - me.width) / 2;
+            a.y = Math.floor(Math.random() * me.height);
+            a.r = Math.floor(Math.random() * me.star_maxR);
+            a.speed = Math.floor(me.star_speed + me.star_speed * Math.random());
+            //            console.log(a.speed)
+            me.stars.push(a);
+        }
+    },
+    new_meteor: function () {
+        var me = this,
+            meteor = {};
+        meteor.x = 100 + (me.width - 20) * Math.random();
+        meteor.y = -80 + (me.height + 80) * Math.random();
+        return meteor;
     },
     loop: function () {
         var me = hoxisora;
-        me.draw_star();
+        me.time = new Date();
+        me.draw_bg();
+
+        //        me.draw_star();
+
+        // 随机触发流星
+        let i = Math.random();
+        if (i > 0.9) {
+            var meteor = me.new_meteor();
+            me.draw_meteor(meteor);
+        }
+
         me.rAF = window.requestAnimationFrame(me.loop);
     },
     draw_bg: function () {
@@ -42,21 +82,45 @@ var hoxisora = {
     },
     draw_star: function () {
         var me = this,
-            time = new Date().getSeconds(),
-            star = [];
+            stars_ = [];
         for (let i = 0; i < me.star_count; i++) {
-            star[i] = me.ctx.createRadialGradient(75, 75, 0, 75, 75, 10);
-            star[i].addColorStop(0, 'rgba(66,232,255,1)');
-            star[i].addColorStop(0.6, 'rgba(66,232,255,.6)');
-            star[i].addColorStop(1, 'rgba(66,232,255,0)');
-
-            me.ctx.fillStyle = star[i];
             me.ctx.save();
             me.ctx.translate(me.width / 2, me.height / 2);
-            me.ctx.rotate(time * 6 * Math.PI / 180);
+            let s0 = 2 * Math.PI / me.stars[i].speed * 60 * 60 * me.time.getHours();
+            let s1 = 2 * Math.PI / me.stars[i].speed * 60 * me.time.getMinutes();
+            let s2 = 2 * Math.PI / me.stars[i].speed * me.time.getSeconds();
+            let s3 = 2 * Math.PI / (me.stars[i].speed * 1000) * me.time.getMilliseconds()
+            me.ctx.rotate(s0 + s1 + s2 + s3);
+            me.ctx.translate(-(me.width / 2), -(me.height / 2));
+            stars_[i] = me.ctx.createRadialGradient(me.stars[i].x, me.stars[i].y, 0, me.stars[i].x, me.stars[i].y, me.stars[i].r);
+            stars_[i].addColorStop(0, 'rgba(66,232,255,1)');
+            stars_[i].addColorStop(0.5, 'rgba(66,232,255,.5)');
+            stars_[i].addColorStop(1, 'rgba(66,232,255,0)');
+
+            me.ctx.fillStyle = stars_[i];
+
+
+            me.ctx.fillRect((me.width - me.height) / 2, 0, me.height, me.height);
             me.ctx.restore();
-            me.ctx.fillRect(0, 0, me.width, me.height);
         }
 
+    },
+    draw_meteor: function (meteor) {
+        var me = this,
+            meteor_;
+        me.ctx.save();
+        meteor_ = me.ctx.createLinearGradient(0, 0, 0, 100); //长100的正方形渐变
+        meteor_.addColorStop(0, 'rgba(48,35,174,.3)');
+        meteor_.addColorStop(1, 'rgba(200,109,215,.3)');
+
+        let s = me.time.getSeconds()
+        me.ctx.translate(meteor.x - s, meteor.y + s);
+
+        me.ctx.rotate(Math.PI / 4)
+        me.ctx.fillStyle = meteor_;
+        me.ctx.fillRect(0, 0, 5, 100); //只显示宽10，长100的界面
+        me.ctx.restore();
+
+        me.meteor_frame = (me.meteor_frame + 1) % 50000000;
     },
 }
