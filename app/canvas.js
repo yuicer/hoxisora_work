ff0c204971704af105f776cc58fd3036072cb300
yuicer <yuicer@yeah.net>
2017-03-16 14:33:43 +0800

    done

--- app/canvas.js
+++ app/canvas.js
@@ -11,15 +11,14 @@ var hoxisora = {
     ctx: {},
     width: {},
     height: {},
-    rAF: {},
     //star
     star_count: 100,
     star_maxR: 2.2,
     star_speed: 200,
     stars: [],
     //meteor
-    meteor_speed: 1,
-    meteor_frame: 0,
+    meteors: [],
+    meteor_speed: 2,
     time: new Date(),
     init: function () {
         var me = this;
@@ -31,11 +30,12 @@ var hoxisora = {
         me.canvas.height = window.innerHeight;
         me.new_star();
 
+        if (/iphone/i.test(navigator.userAgent)) {
+            me.meteor_speed = 5;
+            alert(me.meteor_speed)
+        }
         //anime loop
         me.loop();
-        //        me.draw_bg();
-        //        var meteor = me.new_meteor();
-        //        me.draw_meteor(meteor);
     },
     new_star: function () {
         var me = this;
@@ -45,30 +45,31 @@ var hoxisora = {
             a.y = Math.floor(Math.random() * me.height);
             a.r = Math.floor(Math.random() * me.star_maxR);
             a.speed = Math.floor(me.star_speed + me.star_speed * Math.random());
-            //            console.log(a.speed)
             me.stars.push(a);
         }
     },
     new_meteor: function () {
         var me = this,
             meteor = {};
-        meteor.x = 100 + (me.width - 20) * Math.random();
-        meteor.y = -80 + (me.height + 80) * Math.random();
-        return meteor;
+        meteor.x = Math.floor(me.width / 3 + (me.width / 3 + me.height) * Math.random());
+        meteor.y = -90;
+        meteor.speed = Math.floor(me.meteor_speed / 2 + (3 * me.meteor_speed / 2) * Math.random());
+        console.log(meteor.speed)
+        me.meteors.push(meteor);
     },
     loop: function () {
         var me = hoxisora;
         me.time = new Date();
         me.draw_bg();
-
-        //        me.draw_star();
+        me.draw_star();
+        me.draw_meteor();
 
         // 随机触发流星
         let i = Math.random();
-        if (i > 0.9) {
-            var meteor = me.new_meteor();
-            me.draw_meteor(meteor);
+        if (i > 0.99) {
+            me.new_meteor();
         }
+
 
         me.rAF = window.requestAnimationFrame(me.loop);
     },
@@ -98,29 +99,32 @@ var hoxisora = {
             stars_[i].addColorStop(1, 'rgba(66,232,255,0)');
 
             me.ctx.fillStyle = stars_[i];
-
-
             me.ctx.fillRect((me.width - me.height) / 2, 0, me.height, me.height);
             me.ctx.restore();
         }
 
     },
-    draw_meteor: function (meteor) {
+    draw_meteor: function () {
         var me = this,
-            meteor_;
-        me.ctx.save();
-        meteor_ = me.ctx.createLinearGradient(0, 0, 0, 100); //长100的正方形渐变
-        meteor_.addColorStop(0, 'rgba(48,35,174,.3)');
-        meteor_.addColorStop(1, 'rgba(200,109,215,.3)');
+            meteor_ = [];
+        for (let i = 0; i < me.meteors.length; i++) {
+            me.ctx.save();
+            meteor_[i] = me.ctx.createLinearGradient(0, 0, 0, 100); //长100的正方形渐变
+            meteor_[i].addColorStop(1, 'rgba(66,232,255,.3)');
+            meteor_[i].addColorStop(.7, 'rgba(66,232,255,.1)');
+            meteor_[i].addColorStop(0, 'rgba(66,232,255,.0)');
 
-        let s = me.time.getSeconds()
-        me.ctx.translate(meteor.x - s, meteor.y + s);
 
-        me.ctx.rotate(Math.PI / 4)
-        me.ctx.fillStyle = meteor_;
-        me.ctx.fillRect(0, 0, 5, 100); //只显示宽10，长100的界面
-        me.ctx.restore();
-
-        me.meteor_frame = (me.meteor_frame + 1) % 50000000;
+            me.ctx.translate(me.meteors[i].x, me.meteors[i].y);
+            me.meteors[i].x = me.meteors[i].x - me.meteors[i].speed;
+            me.meteors[i].y = me.meteors[i].y + me.meteors[i].speed;
+            me.ctx.rotate(Math.PI / 4)
+            me.ctx.fillStyle = meteor_[i];
+            me.ctx.fillRect(0, 0, 1, 130); //只显示宽10，长100的界面
+            me.ctx.restore();
+            if (me.meteors[i].x < -10 || me.meteors[i].y > me.height + 10) {
+                me.meteors.splice(i, 1);
+            }
+        }
     },
 }
